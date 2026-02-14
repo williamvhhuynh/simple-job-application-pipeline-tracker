fb4dc3632f851faa82494b2996ae3fbaa8417be4
William Huynh <whuynh02@students.cs.ubc.ca>
2026-02-13 17:29:14 -0800

    Create Status enum

---
+++ src/main/model/Status.java
@@ -0,0 +1,11 @@
+package model;
+
+// Represents the possible states of a job application
+public enum Status {
+    NOT_APPLIED,        
+    APPLIED,            
+    ONLINE_ASSESSMENT,  
+    INTERVIEWING,       
+    OFFERED,            
+    REJECTED            
+}
