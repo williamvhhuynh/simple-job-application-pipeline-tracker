bd231fa1e8e1000e2ad3ecd326df468d64b6411d
William Huynh <whuynh02@students.cs.ubc.ca>
2026-02-15 10:40:49 -0800

    Implement ApplicationPipeline class

--- src/main/model/ApplicationPipeline.java
+++ src/main/model/ApplicationPipeline.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // Represents a collection of job applications 
@@ -10,7 +11,8 @@ public class ApplicationPipeline {
 
     // EFFECTS: creates an empty application pipeline, initializing nextId to 1
     public ApplicationPipeline() {
-        // stub
+        applications = new ArrayList<>();
+        nextId = 1; 
     }
 
     // REQUIRES: company, jobTitle, location != null
@@ -18,7 +20,11 @@ public class ApplicationPipeline {
     // EFFECTS: creates a new JobApplication with a unique Id, adds it to the pipeline,
     //          incrementes nextId, and returns the created application
     public JobApplication addApplication(String company, String jobTitle, String location) {
-        return null;
+        JobApplication newApplication = new JobApplication(nextId, company, jobTitle, location);
+        applications.add(newApplication);
+        nextId++;
+
+        return newApplication;
     }
 
 
@@ -26,34 +32,39 @@ public class ApplicationPipeline {
     // MODIFIES: this
     // EFFECTS: removes the JobApplication with the given id from the pipeline
     public void removeApplication(int id) {
-        // stub
+        applications.remove(getApplicationById(id));
     }
 
     // REQUIRES: application with id exists in the pipeline
     // EFFECTS: returns the JobApplication with the given id
     public JobApplication getApplicationById(int id) {
+        for (JobApplication app : applications) {
+            if (app.getId() == id) {
+                return app;
+            }
+        }
         return null;
     }
 
     // EFFECTS: returns a copy of all applications in this pipeline
     public List<JobApplication> getAllApplications() {
-        return null;
+        return new ArrayList<>(applications);
     }
 
     // EFFECTS: returns number of applications in the pipeline
     public int getApplicationCount() {
-        return -1;
+        return applications.size();
     }
 
     // EFFECTS: returns true if the application with a given id exists in the pipeline,
     //          otherwise returns false
     public boolean containsApplication(int id) {
-        return false;
+        return getApplicationById(id) != null;
     }
 
     // EFFECTS: returns the next id that will be assigned to the new application
     public int getNextId() {
-        return -1;
+        return nextId;
     }
 
 
